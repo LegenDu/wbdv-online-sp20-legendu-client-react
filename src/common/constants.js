1d33da3d410a810ffaf8ab2bf45a7f2fe9cce921
LegenDu <du.leg@husky.neu.edu>
2020-02-04 11:22:06 -0800

    testing https

--- src/common/constants.js
+++ src/common/constants.js
@@ -1,2 +1,1 @@
-export const API_URL = "https://wbdv-generic-server.herokuapp.com/api/001023157/courses"
-export const DEFAULT_CLASS_SIZE = 50
+export const API_URL = "http://wbdv-generic-server.herokuapp.com/api/001023157/courses"
