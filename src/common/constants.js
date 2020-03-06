b20bd2947261eb6b906b87936a9e26b4374efd95
LegenDu <du.leg@husky.neu.edu>
2020-03-06 12:59:05 -0800

    Modified url

--- src/common/constants.js
+++ src/common/constants.js
@@ -4,7 +4,7 @@ export const LESSONS_API_URL = "https://wbdv-generic-server.herokuapp.com/api/00
 export const COURSES_MODULES_API_URL = (courseId) => `https://wbdv-generic-server.herokuapp.com/api/001023157/courses/${courseId}/modules`;
 export const MODULES_LESSONS_API_URL = (moduleId) => `https://wbdv-generic-server.herokuapp.com/api/001023157/modules/${moduleId}/lessons`;
 
-export const ROOT_URL = "http://localhost:8080/api"//"https://wbdv-sp20-javasvr-lgdu-a6.herokuapp.com/api";
+export const ROOT_URL = "https://wbdv-sp20-javasvr-lgdu-a6.herokuapp.com/api";
 export const TOPICS_API_URL = ROOT_URL + "/topics";
 export const WIDGETS_API_URL = ROOT_URL + "/widgets";
-export const LESSON_TOPICS_API_URL = ROOT_URL + "/lessons"
+export const LESSON_TOPICS_API_URL = ROOT_URL + "/lessons";
